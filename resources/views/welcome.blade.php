5978cabbcaa61dd233fc8f26b46ea225e52d80e9
Egert Vreindlik <egert.vreindlik@tptlive.ee>
2024-10-04 13:38:54 +0300

    updated button assignement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="sunset">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +8,10 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-   <button class=" bg-red-600 hover:bg-red-700 ">Danger</button>
-   <div class="border-solid border-2 border-indigo-600 ..."></div>
+    <button class="btn btn-accent">Accent</button>
+    <div> 
+        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Danger</button>
+    </div>
+   
 </body>
 </html>
